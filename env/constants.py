4e4861a4588776d66286fc3890c9a1a2394643c0
halitklyc <halitenes.kalayci@gmail.com>
2025-08-29 13:55:31 +0300

    constants yapısı

---
+++ env/constants.py
@@ -0,0 +1,10 @@
+class Paths():
+    SCREENSHOT = "C:\\Tests\\Screenshots\\"
+
+class URL():
+    BASE_URL = "https://saucedemo.com"
+    INV_URL = f"{BASE_URL}/inventory.html"
+
+class DateFormat():
+    DATE_FORMAT = "%d-%m-%Y"
+    TIME_FORMAT = "%H-%M-%S"
