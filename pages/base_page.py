f93c705b0b192c1d713ba31b0345be90b31c1eff
halitklyc <halitenes.kalayci@gmail.com>
2025-08-29 11:25:44 +0300

    base page eklendi.

---
+++ pages/base_page.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+class BasePage():
+    def __init__(self, driver:WebDriver, wait:WebDriverWait):
+        self.driver = driver
+        self.wait = wait
+
+    def click(self, locator):
+        self.wait.until(expected_conditions.element_to_be_clickable(locator)).click()
+    
+    def send_keys(self, locator, text):
+        self.wait.until(expected_conditions.visibility_of_element_located(locator)).send_keys(text) 
