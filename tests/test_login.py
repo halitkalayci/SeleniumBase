5fadd52df670c81e4c1c75fdf0663bfb6bdec5bc
halitklyc <halitenes.kalayci@gmail.com>
2025-08-29 13:23:52 +0300

    test_login.py eklendi

---
+++ tests/test_login.py
@@ -0,0 +1,8 @@
+from pages.login_page import LoginPage
+
+class TestLogin():
+    def test_successfull(self,driver,wait):
+        login_page = LoginPage(driver, wait)
+        login_page.open()
+        login_page.login("standard_user", "secret_sauce")
+        assert driver.current_url == "https://www.saucedemo.com/inventory.html"
